1ef2fbc04dbc9674f0be35ac8bd385d481840f1f
Rakesh2908 <96054089+Rakesh2908@users.noreply.github.com>
2024-06-15 00:23:17 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -4,7 +4,22 @@ const routes = require("./routes/index");
 
 const app = express()
 
-app.use(cors({ origin: 'https://rakesh-news-aggregator.vercel.app/', credentials: true }));
+const allowedOrigins = [
+  "https://rakesh-news-aggregator.vercel.app",
+  "http://localhost:3000" // Add localhost for development
+];
+
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+};
+
+app.use(cors(corsOptions));
 
 app.use(express.urlencoded({extended: true}))
 
